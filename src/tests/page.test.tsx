3029d0bbd56d9fb4bffd007bcb07c32ecc87ebbc
Hugo Cruz <hugocruzlfc@gmail.com>
2024-12-30 13:47:07 +0100

    feat: add main test

---
+++ src/tests/page.test.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import HomePage from "@/app/page";
+
+describe("Main page test", () => {
+  it("should render the main page", () => {
+    render(<HomePage />);
+    expect(screen.getByText("Counter Test")).toBeDefined();
+  });
+});
